0f04c3088f6688562fd4f56522670addb00a1762
fj-sh <menspocky322@gmail.com>
2021-01-09 10:08:04 +0900

    Created my first blog page.

---
+++ content/blog/hello-world/index.md
@@ -0,0 +1,13 @@
+---
+title: "I started a new blog"
+date: "2020-01-09T01:01:01.284Z"
+category: "self"
+description: "This is my first article."
+emoji: "🐀"
+---
+
+I started a new blog.
+In this blog, I will keep a record of my technology learning.
+
+I'm using catnose's template. Thank you.  
+[CatKnows](https://github.com/catnose99/CatKnows)
---
+++ gatsby-config.js
@@ -0,0 +1,138 @@
+module.exports = {
+  siteMetadata: {
+    title: `Rat Bites Back`,
+    author: `fjsh`,
+    description: `fjsh's personal blog.`,
+    siteUrl: `https://ratbitesback.com`,
+    social: {
+      twitter: `fjsh_app`,
+    },
+    categories: [
+      {
+        name: "Dev",
+        slug: "dev",
+        color: "#f7615f",
+      },
+      {
+        name: "Self",
+        slug: "self",
+        color: "#ffa22b",
+      },
+    ],
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/blog`,
+        name: `blog`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/assets`,
+        name: `assets`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          `gatsby-remark-code-titles`,
+          {
+            resolve: "gatsby-remark-embed-youtube",
+            options: {
+              width: 650,
+              height: 365,
+            },
+          },
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 700,
+              linkImagesToOriginal: false,
+            },
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
+          },
+          // 文字化けするため使わない
+          // {
+          //   resolve: `gatsby-remark-twemoji-shortcut`,
+          //   options: {
+          //     classname: "twemoji"
+          //   }
+          // },
+          {
+            resolve: "gatsby-remark-custom-blocks",
+            options: {
+              blocks: {
+                simple: {
+                  classes: "simple",
+                  title: "optional",
+                },
+                info: {
+                  classes: "info",
+                  title: "optional",
+                },
+                alert: {
+                  classes: "alert",
+                  title: "optional",
+                },
+                notice: {
+                  classes: "notice",
+                  title: "optional",
+                },
+                imageSmall: {
+                  classes: "image-small",
+                },
+                imageMedium: {
+                  classes: "image-medium",
+                },
+              },
+            },
+          },
+          {
+            resolve: `gatsby-remark-prismjs`,
+            options: {
+              classPrefix: "language-",
+              inlineCodeMarker: null,
+              aliases: {},
+              noInlineHighlight: false,
+            },
+          },
+          `gatsby-remark-copy-linked-files`,
+          `gatsby-remark-smartypants`,
+        ],
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Rat Bites Back | fjsh's blog`,
+        short_name: `RatBitesBack`,
+        start_url: `/`,
+        background_color: `rgb(33, 36, 45)`,
+        theme_color: `#0c9ee4`,
+        display: `minimal-ui`,
+        icon: `content/assets/avatar.png`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "",
+      },
+    },
+    `gatsby-plugin-sitemap`,
+    `gatsby-plugin-twitter`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-styled-components`,
+  ],
+};
